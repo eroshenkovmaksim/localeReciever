e16950e3db51721c37dc0dc8ce9080df5e233776
maksim.eroshenkov <maksim.eroshenkov@netcracker.com>
2019-08-01 16:04:01 +0300

    init

---
+++ src/main/java/app/Checker.java
@@ -0,0 +1,23 @@
+package app;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Checker {
+    public static void main(String[] args){
+        try {
+            URL url = new URL("http://localhost:8080/getLocale");
+            URLConnection yc = url.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    yc.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+                System.out.println(inputLine);
+            in.close();
+        } catch(IOException e){
+        }
+    }
+}
---
+++ src/main/java/app/RequesterController.java
@@ -0,0 +1,17 @@
+package app;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.Locale;
+
+
+@RestController
+public class RequesterController {
+
+    @RequestMapping("/getLocale")
+    @ResponseBody
+    public String handleRequest (Locale locale) {
+        return String.format("Language: %s, Country: %s %n",
+                locale.getDisplayLanguage(), locale.getDisplayCountry());
+    }
+
+}
